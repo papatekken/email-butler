7b3beaf77868245e95d7675afd3abc8294d53dc0
Allan Yip <papatekken@gmail.com>
2026-05-05 01:32:44 +0100

    init project

---
+++ src/main/java/com/example/email_butler/EmailButlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.email_butler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailButlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailButlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/email_butler/EmailButlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.email_butler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailButlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
